70eedf99fcf43563d26df151b2be79584c13e827
joemiddie13 <joemiddie13@gmail.com>
2024-02-21 19:11:19 -0800

    Added init file to set up flask login

---
+++ grocery_app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from .models import User
+from grocery_app.config import Config
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
